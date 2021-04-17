c452215d0f4f8b3a5b584e8938bc874573d0e5fd
Rob <r.jacobs790@gmail.com>
2021-04-16 22:25:54 -0400

    Did whole program. Forgot to commit while doing it, but it works.

---
+++ Develop/README.md
@@ -0,0 +1,35 @@
+# There's a title?
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  ## Description
+  I would but it's top secret
+  ## Table of Contents
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [Contribution](#Contribution)
+  * [Test](#Test)
+  * [Links](#Links)
+  ## Installation
+  You run the program.
+  ## Usage
+  I told you, It's top secret.
+  ## Contribution
+  me and only me. that's all you need to know.
+  ## License
+  (https://opensource.org/licenses/Apache-2.0)
+  Copyright [yyyy] [name of copyright owner]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+ 
+      http://www.apache.org/licenses/LICENSE-2.0
+ 
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+  ## Test
+  Stop asking me these personal questions.
+  ## Questions
+  If you have any questions about the repo, open an issue or contact me directly at Now that's definitly too personal.. You can find more of my work at https://github.com/JakeRoberts
---
+++ Develop/index.js
@@ -0,0 +1,70 @@
+// TODO: Include packages needed for this application
+// uses node package manager to bring in
+const inquirer = require("inquirer");
+// core module
+const fs = require('fs');
+
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        message: "Give a description on you project:",
+        name: "description",
+    },
+    {
+        message: "How do you install the project?",
+        name: "installation",
+    },
+    {
+        message: "Give a brief summary on how to use your project:",
+        name: "usage",
+    },
+    {
+        message: "Provide information on how you can test all code:",
+        name: "test",
+    },
+    {
+        message: "Provide contributting guidelines:",
+        name: "contribution",
+    },
+    {
+        type: "list",
+        message: "Licensing information:",
+        name: "license",
+        choices: ["MIT", "Apache", "GNU"],
+    },
+    {
+        message: "Provide GitHub Username:",
+        name: "GitHub",
+    },
+    {
+        message: "Provide E-mail address:",
+        name: "email",
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    //takes in filename and data
+    fs.writeFileSync(fileName, data);
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    //loads in questions to be answered in order. THEN will send answeres to another function
+    inquirer.prompt(questions).then(handleResponses);
+}
+
+function handleResponses(response) {
+const md = generateMarkdown(response);
+writeToFile("README.md", md);
+console.log("Thank you for using this app.");
+}
+
+// Function call to initialize app
+init();
